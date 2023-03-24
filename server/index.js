a0d347c78b327b01a37c56596fa336e9a61d63e8
Balaguru <balagurusiva2002@gmail.com>
2023-03-24 17:20:58 +0530

    modified:   server/index.js new file:   server/models/User.js

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan"; 
 import path from "path";
 import { fileURLToPath } from "url";
+import {register} from './controllers/auth.js'
 
 //configuration
 const __filename = fileURLToPath(import.meta.url);
@@ -34,6 +35,9 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({storage});
+
+//routes with file
+app.post("/auth/register", upload.single('picture'), register)
 
 //mongoose setup
 const MONGO_URI = 'mongodb+srv://Balaguru:Bala%402002@nodeapi.l7sfb.mongodb.net/?retryWrites=true&w=majority'
