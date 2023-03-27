02861a30aab9fd5626555a6ec741d0a48f05d74b
Balaguru <balagurusiva2002@gmail.com>
2023-03-27 18:41:29 +0530

    new file:   client/jsconfig.json 	modified:   client/src/App.js 	new file:   client/src/scenes/homePage/index.jsx 	new file:   client/src/scenes/loginPage/index.jsx 	new file:   client/src/scenes/navbar/index.jsx 	new file:   client/src/scenes/profilePage/index.jsx 	new file:   client/src/state/index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
- 
+import { BrowserRouter,  Route, Routes } from 'react-router-dom'; 
 import './App.css';
+import LoginPage from './scenes/loginPage';
+import ProfilePage from './scenes/profilePage';
+import HomePage from './scenes/homePage';
 
 function App() {
   return (
     <div className="app">
-      hello
+       <BrowserRouter>
+         <Routes>
+          <Route path = "/" element={<LoginPage />} />
+          <Route path = "/home" element={<HomePage />} />
+          <Route path = "/profile/:userId" element={<ProfilePage />} />
+         </Routes>
+       </BrowserRouter>
      
     </div>
   );
---
+++ client/src/scenes/loginPage/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LoginPage() {
+  return (
+    <div>
+     login page
+    </div>
+  )
+}
+
+export default LoginPage
