ec243f6721fa1dbfc36d66a1d1559c9fab064860
Balaguru <balagurusiva2002@gmail.com>
2023-03-30 10:39:07 +0530

    modified:   client/src/state/index.js

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -13,38 +13,40 @@ export const authSlice = createSlice({
     reducer:{
         setMode: (state)=>{
             state.mode = state.mode === "light" ? "dark" : "light";
+        },
+
+        setLogin: (state,action) =>{
+            state.user = action.payload.user;
+            state.token = action.payload.token;
+        },
+    
+        setLogout: (state,action) => {
+            state.user = null;
+            state.token = null;
+        },
+    
+        setFriends: (state,action) => {
+            if(state.user){
+                state.user.friends = action.payload.friends;
+            }else{
+                console.error("user friends has no friends");
+            }
+        },
+    
+        setPosts:(state,action) =>{
+            state.posts = action.payload.posts;
+        },
+    
+        setPost: (state,action) =>{
+            const updatedPost = state.posts.map((post) =>{
+                if(post._id === action.payload.post._id) return action
+                return post; 
+            });
+            state.posts = updatedPost;
         }
-    },
-
-    setLogin: (state,action) =>{
-        state.user = action.payload.user;
-        state.token = action.payload.token;
-    },
-
-    setLogout: (state,action) => {
-        state.user = null;
-        state.token = null;
-    },
-
-    setFriends: (state,action) => {
-        if(state.user){
-            state.user.friends = action.payload.friends;
-        }else{
-            console.error("user friends has no friends");
-        }
-    },
-
-    setPosts:(state,action) =>{
-        state.posts = action.payload.posts;
-    },
-
-    setPost: (state,action) =>{
-        const updatedPost = state.posts.map((post) =>{
-            if(post._id === action.payload.post._id) return action
-            return post; 
-        });
-        state.posts = updatedPost;
     }
+
+     
 });
 
 
